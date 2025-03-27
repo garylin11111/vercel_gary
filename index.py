f0412c5efcc936c16ba49525c59da8e9df8b4e1a
garylin11111 <garry240993@gmail.com>
2025-03-27 10:11:41 +0800

    v2

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ def index():
 	homepage = "<h1>林政彥Python網頁(時間+8)</h1>"
 	homepage +="<a href=/mis>MIS</a><br>"
 	homepage +="<a href=/today>顯示日期時間</a><br>"
-	homepage +="<a href=/welcome?nick=政彥>傳送使用者暱稱</a><br>"
+	homepage +="<a href=/welcome?nick=政彥&work=pu>傳送使用者暱稱</a><br>"
 	homepage +="<a href=/account>網頁表單傳值</a><br>"
 	homepage +="<a href=/rwd>政彥簡介網頁</a><br>"
 	return homepage
@@ -32,6 +32,7 @@ def me():
 @app.route("/welcome", methods=["GET"])
 def welcome():
     user = request.values.get("nick")
+    w = request.values.get("work")
     return render_template("welcome.html", name=user)
 
 @app.route("/account", methods=["GET", "POST"])
